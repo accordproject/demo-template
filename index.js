3a7edf523ba62af2eab0f3d34f266aa33decf7fe
Dan Selman <dselman@meta.com>
2025-04-02 09:48:13 +0100

    added draft
    Signed-off-by: Dan Selman <dselman@meta.com>

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const { TemplateArchiveProcessor } = require('@accordproject/template-engine');
 const { Template } = require('@accordproject/cicero-core');
 
-async function main() {
-    const template = await Template.fromDirectory('./archives/latedeliveryandpenalty-typescript');
+const TEMPLATE_PATH = './archives/latedeliveryandpenalty-typescript';
+
+async function trigger() {
+    const template = await Template.fromDirectory(TEMPLATE_PATH);
     const templateArchiveProcessor = new TemplateArchiveProcessor(template);
     const data = {
         "$class": "io.clause.latedeliveryandpenalty@0.1.0.TemplateModel",
@@ -30,4 +32,32 @@ async function main() {
     console.log(JSON.stringify(response, null, 2));
 }
 
-main();
+async function draft() {
+    const template = await Template.fromDirectory(TEMPLATE_PATH);
+    const templateArchiveProcessor = new TemplateArchiveProcessor(template);
+    const data = {
+        "$class": "io.clause.latedeliveryandpenalty@0.1.0.TemplateModel",
+        "forceMajeure": true,
+        "penaltyDuration": {
+            "$class": "org.accordproject.time@0.3.0.Duration",
+            "amount": 2,
+            "unit": "days"
+        },
+        "penaltyPercentage": 10.5,
+        "capPercentage": 55,
+        "termination": {
+            "$class": "org.accordproject.time@0.3.0.Duration",
+            "amount": 15,
+            "unit": "days"
+        },
+        "fractionalPart": "days",
+        "clauseId": "c88e5ed7-c3e0-4249-a99c-ce9278684ac8",
+        "$identifier": "c88e5ed7-c3e0-4249-a99c-ce9278684ac8"
+    };
+    const options = {verbose: true};
+    const result = await templateArchiveProcessor.draft(data, 'markdown', options);
+    console.log(result);
+}
+
+// trigger();
+draft();
